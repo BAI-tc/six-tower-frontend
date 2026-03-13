adecb87d013f2a17abfc2c93d10207fcf3d255cc
BAI_tc <857311817@qq.com>
2026-03-13 22:12:09 +0800

    fix: update API domain and fix netlify config

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,6 @@
 import Header from '@/app/_components/header';
 import Footer from '@/app/_components/footer';
-import { Inter } from 'next/font/google';
 import './globals.css';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'SixTower',
@@ -16,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body className={inter.className + ' flex flex-col min-h-screen'}>
+      <body className='flex flex-col min-h-screen'>
         <Header />
         <main className='container mx-auto grow'>{children}</main>
         <Footer />
